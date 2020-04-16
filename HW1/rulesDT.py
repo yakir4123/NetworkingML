b39b2548674ce2cc0250031770b39ff46506d7c5
yakir4123 <38066978+yakir4123@users.noreply.github.com>
2020-04-16 22:06:23 +0300

    Use IG criteria as minimum entropy, better performance than previous, save to file the DT

--- HW1/rulesDT.py
+++ HW1/rulesDT.py
@@ -1,15 +1,20 @@
 import gc
 import math
+import time
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+
 from netaddr import *
 
+from HW1 import utils
+
 NUM_OF_BITS = 64
-GROUP_NUM = 64
+GROUP_NUM = 128
 
 rules_df = pd.read_csv('rule01.tsv', sep=' ', index_col=False, header=None,
                        names=['src_ip', 'dst_ip', 'src_port', 'dst_port', 'prot'])
+rules_df = rules_df.loc[:400]
 rules_df = rules_df.drop(columns=['src_port', 'dst_port', 'prot'])
 rules_df['src_ip'] = rules_df['src_ip'].apply(lambda s: s[1:])
 
@@ -37,10 +42,18 @@ del rules_df['dst_ip_rule']
 gc.collect()
 
 
-def get_rules_by_condition(rules, conditions):
-    for b_i, value in conditions.items():
-        rules = rules.loc[(rules['rule'].str[b_i] == value) |
-                                (rules['rule'].str[b_i] == '*')]
+def get_rules_by_condition(rules, condition):
+    rules = rules.loc[(rules['rule'].str[condition[0]] == condition[1]) |
+                      (rules['rule'].str[condition[0]] == '*')]
+    return rules
+
+
+def set_rule_wildcards_bit(rules, condition):
+    rules.loc[(rules['rule'].str[condition[0]] == '*'), 'rule_power'] //= 2
+    rules.loc[(rules['rule'].str[condition[0]] == '*'), 'rule'] =\
+        rules.loc[(rules['rule'].str[condition[0]] == '*'), 'rule'].str[:condition[0]]\
+        + condition[1]\
+        + rules.loc[(rules['rule'].str[condition[0]] == '*'), 'rule'].str[condition[0]+1:]
     return rules
 
 
@@ -60,56 +73,61 @@ def conditional_entropy(rules, conditions=None):
         if p == 1 or p == 0:
             return 0
         return -p * math.log(p, 2) - (1 - p) * math.log(1 - p, 2)
-
+    if conditions is not None:
+        rules = set_rule_wildcards_bit(rules, conditions)
     return [entropy(p) for p in P], rules
 
 
-gains, _ = conditional_entropy(rules_df, {})
-decision_tree = nx.Graph()
-first_node = gains.index(max(gains))
-decision_tree.add_node(str(first_node))
+node_counter = 0
 
 
-def ip_view(d):
+def node_name(tup):
+    global node_counter
+    node_counter += 1
+    return '{node_counter}:b_{i}={val}'\
+        .format(i=tup[0], val=tup[1], node_counter=node_counter)
 
-    return ''.join(list(map(lambda k: d[k] if k in d.keys() else '_', range(64))))
 
-
-def add_nodes(last_node, conditions, which_to_check, to_connect, rules):
-
+def add_nodes(last_node, which_to_check, to_connect, rules):
     if len(rules) <= GROUP_NUM:
         return
 
-    which_to_check[last_node] = 0 #[0,0,0,0,1,1,1,1,1....
-    prior_knowledge_zero = conditions.copy()
-    prior_knowledge_one = conditions.copy()
-    prior_knowledge_zero[last_node] = '0'
-    prior_knowledge_one[last_node] = '1'
-    
-    gain_zero, rules_zero = conditional_entropy(rules, prior_knowledge_zero)
-    gain_zero = [a * b for a, b in zip(gain_zero, which_to_check)] #[0,0,1,1...  gain_zero = [0, 0, 0.7...
-    zero_node = gain_zero.index(max(gain_zero))
-    gain_one, rules_one = conditional_entropy(rules, prior_knowledge_one)
-    gain_one = [a * b for a, b in zip(gain_one, which_to_check)]
-    one_node = gain_one.index(max(gain_one))
+    which_to_check[last_node] = 0  # [0,0,0,0,1,1,1,1,1....
+    prior_knowledge_zero = (last_node, '0')
+    prior_knowledge_one = (last_node, '1')
 
-    zero_node_str = ip_view(prior_knowledge_zero)
+    gain_zero, rules_zero = conditional_entropy(rules, prior_knowledge_zero)
+    gain_zero = [-a * b for a, b in zip(gain_zero, which_to_check)]  # [0,0,1,1...  gain_zero = [0, 0, 0.7...
+    zero_node = gain_zero.index(min(gain_zero))
+    gain_one, rules_one = conditional_entropy(rules, prior_knowledge_one)
+    gain_one = [-a * b for a, b in zip(gain_one, which_to_check)]
+    one_node = gain_one.index(min(gain_one))
+
+    zero_node_str = node_name(prior_knowledge_zero)
     print(zero_node_str)
     zero_edge = (to_connect, zero_node_str)
     decision_tree.add_edge(*zero_edge, object="{ " + str(last_node) + " : 0")
-    one_node_str = ip_view(prior_knowledge_one)
+    one_node_str = node_name(prior_knowledge_one)
     print(one_node_str)
     one_edge = (to_connect, one_node_str)
     decision_tree.add_edge(*one_edge, object="{ " + str(last_node) + " : 1")
 
-    add_nodes(zero_node, prior_knowledge_zero.copy(), which_to_check.copy(), zero_node_str, rules_zero)
-    add_nodes(one_node, prior_knowledge_one.copy(), which_to_check.copy(), one_node_str, rules_one)
+    add_nodes(zero_node, which_to_check.copy(), zero_node_str, rules_zero)
+    add_nodes(one_node, which_to_check.copy(), one_node_str, rules_one)
 
+
+start_time = time.time()
+gains, _ = conditional_entropy(rules_df)
+decision_tree = nx.Graph()
+first_node = gains.index(min(gains))
+decision_tree.add_node(str(first_node))
 
 to_check = [1] * 64
-add_nodes(first_node, {}, to_check, str(first_node), rules_df)
-# add_nodes(first_node, {}, to_check, str(first_node), rules_df)
+add_nodes(first_node, to_check, str(first_node), rules_df)
 
+pos = utils.hierarchy_pos(decision_tree, 1)
+nx.draw(decision_tree, pos, with_labels=True)
 
-nx.draw(decision_tree, with_labels=True)
+nx.write_adjlist(decision_tree, "decision_tree_{}".format(GROUP_NUM))
 plt.show()
+print("--- %s seconds ---" % (time.time() - start_time))
