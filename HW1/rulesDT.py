3b53d6b842b3cacf9d05d7c95ad4c56d5da656e1
yakir4123 <38066978+yakir4123@users.noreply.github.com>
2020-04-16 19:47:49 +0300

    One tree, still bugged that calculate by max entropy and not min entropy

--- HW1/rulesDT.py
+++ HW1/rulesDT.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from netaddr import *
 
 NUM_OF_BITS = 64
-GROUP_NUM = 16
+GROUP_NUM = 64
 
 rules_df = pd.read_csv('rule01.tsv', sep=' ', index_col=False, header=None,
                        names=['src_ip', 'dst_ip', 'src_port', 'dst_port', 'prot'])
@@ -51,8 +51,8 @@ def conditional_entropy(rules, conditions=None):
     total_rule_power = rules['rule_power'].sum()
     for bi in range(0, 64):
         try:
-            P[bi] = (rules_df.loc[(rules['rule'].str[bi] == '0'), 'rule_power'].sum() +
-                     rules_df.loc[(rules['rule'].str[bi] == '*'), 'rule_power'].sum() / 2) / total_rule_power
+            P[bi] = (rules.loc[(rules['rule'].str[bi] == '0'), 'rule_power'].sum() +
+                     rules.loc[(rules['rule'].str[bi] == '*'), 'rule_power'].sum() / 2) / total_rule_power
         except ZeroDivisionError:
             P[bi] = 0
 
@@ -64,7 +64,7 @@ def conditional_entropy(rules, conditions=None):
     return [entropy(p) for p in P], rules
 
 
-gains = conditional_entropy(rules_df, {})
+gains, _ = conditional_entropy(rules_df, {})
 decision_tree = nx.Graph()
 first_node = gains.index(max(gains))
 decision_tree.add_node(str(first_node))
@@ -86,30 +86,29 @@ def add_nodes(last_node, conditions, which_to_check, to_connect, rules):
     prior_knowledge_zero[last_node] = '0'
     prior_knowledge_one[last_node] = '1'
     
-    gain_zero, rules = conditional_entropy(rules, prior_knowledge_zero)
+    gain_zero, rules_zero = conditional_entropy(rules, prior_knowledge_zero)
     gain_zero = [a * b for a, b in zip(gain_zero, which_to_check)] #[0,0,1,1...  gain_zero = [0, 0, 0.7...
     zero_node = gain_zero.index(max(gain_zero))
-    gain_one, rules = conditional_entropy(rules, prior_knowledge_one)
+    gain_one, rules_one = conditional_entropy(rules, prior_knowledge_one)
     gain_one = [a * b for a, b in zip(gain_one, which_to_check)]
     one_node = gain_one.index(max(gain_one))
 
     zero_node_str = ip_view(prior_knowledge_zero)
     print(zero_node_str)
     zero_edge = (to_connect, zero_node_str)
-    decision_tree.add_edge(*zero_edge, object = "{ " + str(last_node) + " : 0")
+    decision_tree.add_edge(*zero_edge, object="{ " + str(last_node) + " : 0")
     one_node_str = ip_view(prior_knowledge_one)
     print(one_node_str)
     one_edge = (to_connect, one_node_str)
-    decision_tree.add_edge(*one_edge, object = "{ " + str(last_node) + " : 1")
+    decision_tree.add_edge(*one_edge, object="{ " + str(last_node) + " : 1")
 
-    add_nodes(zero_node, prior_knowledge_zero.copy(), which_to_check.copy(), zero_node_str, rules)
-    add_nodes(one_node, prior_knowledge_one.copy(), which_to_check.copy(), one_node_str, rules)
+    add_nodes(zero_node, prior_knowledge_zero.copy(), which_to_check.copy(), zero_node_str, rules_zero)
+    add_nodes(one_node, prior_knowledge_one.copy(), which_to_check.copy(), one_node_str, rules_one)
 
 
 to_check = [1] * 64
-to_check[first_node] = 0
-add_nodes(first_node, {}, to_check, str(first_node), 0, rules_df)
-add_nodes(first_node, {}, to_check, str(first_node), 0, rules_df)
+add_nodes(first_node, {}, to_check, str(first_node), rules_df)
+# add_nodes(first_node, {}, to_check, str(first_node), rules_df)
 
 
 nx.draw(decision_tree, with_labels=True)
